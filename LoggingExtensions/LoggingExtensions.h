8451a48dd027a02c47925aea94c2d79cf855c2aa
yu840915 <yu840915@gmail.com>
2023-06-27 11:23:09 +0800

    Initial Commit

---
+++ LoggingExtensions/LoggingExtensions.h
@@ -0,0 +1,18 @@
+//
+//  LoggingExtensions.h
+//  LoggingExtensions
+//
+//  Created by 立宣于 on 2023/6/27.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LoggingExtensions.
+FOUNDATION_EXPORT double LoggingExtensionsVersionNumber;
+
+//! Project version string for LoggingExtensions.
+FOUNDATION_EXPORT const unsigned char LoggingExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LoggingExtensions/PublicHeader.h>
+
+
